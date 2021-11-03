498f9b4524147e63bb513df5b8dc732bac47c30f
penny.liu <penny.liu@kaiostech.com>
2021-11-03 15:42:15 +0800

    Computed Properties

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ const app = Vue.createApp({
       book.isFav = !book.isFav;
     },
   },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav);
+    },
+  },
 });
 
 app.mount('#app');
