44a3ddb2a42d2cbb4f84cadb5ea243ce51d3dbec
penny.liu <penny.liu@kaiostech.com>
2021-11-06 17:20:10 +0800

    Template Refs

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">click me</button>
 </template>
 
 <script>
@@ -9,6 +11,13 @@ export default {
     return {
       title: "My First Vue App :)",
     };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
   },
 };
 </script>
