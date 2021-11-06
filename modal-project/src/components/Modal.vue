e82a227d3f32ef81c9e955481754ee13985d4192
penny.liu <penny.liu@kaiostech.com>
2021-11-06 22:31:21 +0800

    Component Styles & Global Styles

--- modal-project/src/components/Modal.vue
+++ modal-project/src/components/Modal.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="backdrop">
     <div class="modal">
+      <h1>Modal Title</h1>
       <p>modal content</p>
     </div>
   </div>
@@ -20,5 +21,13 @@
   background: rgba(0, 0, 0, 0.5);
   width: 100%;
   height: 100%;
+}
+.modal h1 {
+  color: #03cfb4;
+  border: none;
+  padding: 0;
+}
+.modal p {
+  font-style: normal;
 }
 </style>
