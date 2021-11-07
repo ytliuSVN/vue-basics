bda6d8a3f9e085836b05bcdfa6601e1ef50068d3
penny.liu <penny.liu@kaiostech.com>
2021-11-07 22:04:57 +0800

    Click Event Modifiers

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -4,7 +4,7 @@
   <div v-if="showModal">
     <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
   </div>
-  <button @click="toggleModal">open modal</button>
+  <button @click.alt="toggleModal">open modal (alt)</button>
   <!-- <input type="text" ref="name" />
   <button @click="handleClick">click me</button> -->
 </template>
