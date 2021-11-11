672a0106431be9ea9e8815e168d35200cb31982f
penny.liu <penny.liu@kaiostech.com>
2021-11-11 16:17:46 +0800

    Using Teleport

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
-  <div v-if="showModal">
+  <teleport to="#modals" v-if="showModal">
     <Modal theme="sale" @close="toggleModal">
       <template #links>
         <a href="#">sign up now</a>
@@ -12,16 +12,16 @@
         <p>{{ text }}</p>
       </template>
     </Modal>
-  </div>
+  </teleport>
 
-  <div v-if="showModalTwo">
+  <teleport to="#modals" v-if="showModalTwo">
     <Modal @close="toggleModalTwo">
       <template #default>
         <h1>Sign up to the Newsletter</h1>
         <p>For updates and promo codes!</p>
       </template>
     </Modal>
-  </div>
+  </teleport>
 
   <button @click.alt="toggleModal">open modal (alt)</button>
   <button @click="toggleModalTwo">open modal</button>
@@ -61,7 +61,8 @@ export default {
 </script>
 
 <style>
-#app {
+#app,
+#modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
