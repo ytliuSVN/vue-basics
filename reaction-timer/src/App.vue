d65785e6322fd3e1ed085bcd5c5ce8ab89ae412d
penny.liu <enderman542@gmail.com>
2021-11-13 13:00:08 +0800

    Starting a New Game

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
+import Block from "./components/Block.vue";
+
 export default {
-  name: 'App',
-  components: {
-  }
-}
+  name: "App",
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      // between two and seven seconds
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    },
+  },
+};
 </script>
 
 <style>
