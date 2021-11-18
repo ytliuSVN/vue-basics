bfcc98a158572fe7aafc6aa472121f9efefa66c0
penny.liu <enderman542@gmail.com>
2021-11-18 10:28:06 +0800

    Component Lifecycle Hooks

--- reaction-timer/src/components/Block.vue
+++ reaction-timer/src/components/Block.vue
@@ -1,10 +1,28 @@
 <template>
-  <div class="block">click me</div>
+  <div class="block" v-if="showBlock">click me</div>
 </template>
 
 <script>
 export default {
   props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  mounted() {
+    // console.log('component mounted');
+    setTimeout(() => {
+      this.showBlock = true;
+      console.log(this.delay);
+    }, this.delay);
+  },
+  updated() {
+    console.log("component updated");
+  },
+  unmounted() {
+    console.log("unmounted");
+  },
 };
 </script>
 
