a19f6fa254bbab61155dd53d5f65dee4201ad76e
penny.liu <enderman542@gmail.com>
2021-11-24 11:09:12 +0800

    Deleting Skills

--- web-forms/src/components/SignupForm.vue
+++ web-forms/src/components/SignupForm.vue
@@ -15,7 +15,7 @@
     <label>Skills (press alt + comma to add):</label>
     <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
     <div v-for="skill in skills" :key="skill" class="pill">
-      {{ skill }}
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
     </div>
 
     <div class="terms">
@@ -66,6 +66,9 @@ export default {
         this.tempSkill = "";
       }
     },
+    deleteSkill(skill) {
+      this.skills = this.skills.filter((item) => item !== skill);
+    },
   },
 };
 </script>
@@ -104,5 +107,17 @@ input[type="checkbox"] {
   margin: 0 10px 0 0;
   position: relative;
   top: 2px;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
 }
 </style>
