95c01c40faca055dbdf0a956b3dac7c76e37861a
penny.liu <enderman542@gmail.com>
2021-11-25 22:07:37 +0800

    Router Links

--- ninja-jobs/src/App.vue
+++ ninja-jobs/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :to="{ name: 'About' }">About</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
@@ -22,9 +22,13 @@
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 4px;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: white;
+  background: crimson;
 }
 </style>
--- ninja-jobs/src/views/About.vue
+++ ninja-jobs/src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ad veniam
+      totam magni maiores aperiam vel possimus harum laboriosam placeat, dolorum
+      eaque architecto aspernatur ipsa adipisci. Harum odit autem voluptatem
+      placeat illo nulla consequuntur exercitationem temporibus reprehenderit
+      deleniti quasi nisi, vero doloribus! Reprehenderit, dicta. At quam
+      asperiores excepturi, ducimus accusantium animi pariatur maiores commodi
+      porro ratione voluptatem molestias nesciunt tempora ad illo sunt. Illo
+      ducimus quae eos obcaecati minima ab consectetur, magnam laudantium aut
+      similique illum quidem ullam odit nulla ipsam laborum sed dolorem sit!
+      Necessitatibus cupiditate facilis, corporis, autem laudantium tenetur
+      veritatis blanditiis sint distinctio atque a porro id!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi impedit
+      adipisci omnis nulla deleniti est incidunt quam soluta amet magni, debitis
+      aliquam facilis earum. Sit accusamus esse provident doloremque enim nobis
+      modi voluptate? Sint quo, quidem ipsum possimus adipisci facere nemo, quas
+      hic unde eius quia, sequi rerum ratione quos commodi mollitia. Quaerat
+      odit, ab repellendus voluptatibus autem soluta! Ea molestias neque
+      corrupti vitae odio voluptate, voluptates sequi quidem iusto omnis aliquam
+      ex cupiditate culpa sunt quod! Perspiciatis harum in doloremque porro
+      repudiandae accusamus vero, illo aliquam quod saepe voluptatum tempora,
+      unde asperiores recusandae corporis dolorum impedit. Ratione, doloribus
+      sit?
+    </p>
   </div>
 </template>
