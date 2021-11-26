8690f91a5574f89ed5c3b6d4803b06592db35424
penny.liu <enderman542@gmail.com>
2021-11-26 22:22:15 +0800

    404 Pages & Redirects

--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue';
 import About from '../views/About.vue';
 import Jobs from '../views/jobs/Jobs.vue';
 import JobDetails from '../views/jobs/JobDetails.vue';
+import NotFound from '../views/NotFound.vue';
 
 const routes = [
   {
@@ -25,6 +26,17 @@ const routes = [
     name: 'JobDetails',
     component: JobDetails,
     props: true,
+  },
+  // redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs',
+  },
+  // catchall 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound,
   },
 ];
 
---
+++ ninja-jobs/src/views/NotFound.vue
@@ -0,0 +1,4 @@
+<template>
+  <h2>404</h2>
+  <h3>That page does not exist</h3>
+</template>
