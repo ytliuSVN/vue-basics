a7f8d2325f07796be34faf103b80a90380d5fff9
penny.liu <enderman542@gmail.com>
2021-11-26 10:13:12 +0800

    Folder Structure

--- ninja-jobs/src/App.vue
+++ ninja-jobs/src/App.vue
@@ -2,6 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link :to="{ name: 'About' }">About</router-link>
+    <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </div>
   <router-view />
 </template>
