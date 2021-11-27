bd7f769c5b65aee295bc5dd095927bc099164ed1
penny.liu <enderman542@gmail.com>
2021-11-27 10:20:44 +0800

    Lazy Loading Components

--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
-import About from '../views/About.vue';
+// import About from '../views/About.vue';
 import Jobs from '../views/jobs/Jobs.vue';
 import JobDetails from '../views/jobs/JobDetails.vue';
 import NotFound from '../views/NotFound.vue';
@@ -14,7 +14,7 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    component: About,
+    component: () => import('../views/About.vue'),
   },
   {
     path: '/jobs',
