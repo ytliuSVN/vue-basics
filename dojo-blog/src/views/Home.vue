7a86c5ca4d1fe23b9ddda4fe697e0038ab837373
Penny Liu <pennyliu@Pennys-MBP.hitronhub.home>
2021-12-04 11:16:40 +0800

    Composition API Basics

---
+++ dojo-blog/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  setup() {
+    console.log("setup");
+    let name = "mario";
+    let age = "54";
+
+    const handleClick = () => {
+      console.log("you clicked me");
+    };
+
+    return { name, age, handleClick };
+  },
+};
+</script>
