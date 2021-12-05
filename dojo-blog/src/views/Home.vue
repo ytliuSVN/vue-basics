512d43be70e5e3c5d7425fbf6f150e43a37e094c
penny.liu <im.penny@protonmail.com>
2021-12-05 15:10:31 +0800

    Template Refs in the Setup Function

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="home">
     home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "Home",
   setup() {
-    console.log("setup");
+    console.log(this);
+
+    const p = ref("hello");
+
     let name = "mario";
     let age = "54";
 
     const handleClick = () => {
-      console.log("you clicked me");
+      console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.textContent = "hello, penny";
     };
 
-    return { name, age, handleClick };
+    return { name, age, handleClick, p };
   },
 };
 </script>
