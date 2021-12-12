c896f019bf69b33a80923937fcd536556aa5b2ff
penny.liu <im.penny@protonmail.com>
2021-12-12 21:28:28 +0800

    Creating Another Composable

--- dojo-blog/src/composables/getPosts.js
+++ dojo-blog/src/composables/getPosts.js
@@ -8,11 +8,10 @@ const getPosts = () => {
     try {
       // read our JSON
       let data = await fetch('http://localhost:3000/posts');
-      posts.value = await data.json();
-
       if (!data.ok) {
         throw Error('no available data');
       }
+      posts.value = await data.json();
     } catch (err) {
       error.value = err.message;
       console.log(error.value);
--- dojo-blog/src/router/index.js
+++ dojo-blog/src/router/index.js
@@ -1,17 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import Details from '../views/Details.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  }
-]
+    component: Home,
+  },
+  {
+    path: '/posts/:id',
+    name: 'Details',
+    component: Details,
+    props: true,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
