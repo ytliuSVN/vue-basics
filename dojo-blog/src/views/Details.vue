e3b6502e6d0c083f45baad44c521feb09efc5dba
penny.liu <im.penny@protonmail.com>
2021-12-14 20:53:32 +0800

    Creating a Loading Spinner

--- dojo-blog/src/views/Details.vue
+++ dojo-blog/src/views/Details.vue
@@ -4,14 +4,17 @@
     <h3>{{ post.title }}</h3>
     <p class="pre">{{ post.body }}</p>
   </div>
-  <div v-else>Loading...</div>
+  <div v-else><Spinner /></div>
 </template>
 
 <script>
 import getPost from "../composables/getPost";
+import Spinner from "../components/Spinner.vue";
 
 export default {
+  name: "Details",
   props: ["id"],
+  components: { Spinner },
   setup(props) {
     const { post, error, load } = getPost(props.id);
     load();
--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -5,17 +5,20 @@
     <div v-if="posts.length">
       <PostList :posts="posts" />
     </div>
-    <div v-else>Loading...</div>
+    <div v-else>
+      <Spinner />
+    </div>
   </div>
 </template>
 
 <script>
 import PostList from "../components/PostList.vue";
 import getPosts from "../composables/getPosts";
+import Spinner from "../components/Spinner.vue";
 
 export default {
   name: "Home",
-  components: { PostList },
+  components: { PostList, Spinner },
   setup() {
     const { posts, error, load } = getPosts();
     load();
